744e6a4b007d292fcbb94feeb72bb6418f72b42d
Elijah Bretbacher <elijah.bretbacher@htl-wels.at>
2020-10-08 12:36:10 +0200

    modified for an optional multiplication

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,12 @@ public class Main {
         float x;
         float y;
         float z;
-        float result;
+        float result=-1;
+        float mult;
+        String s;
         Scanner sc=new Scanner(System.in);
 
-        System.out.println("Please enter 2 Numbers to add!");
+        System.out.println("Please enter 3 Numbers to add!");
         try {
             x = sc.nextFloat();
             y = sc.nextFloat();
@@ -19,5 +21,21 @@ public class Main {
         }catch(Exception e) {
             System.out.println("An error got detected");
         }
+
+        System.out.println("Do you want to multiply it by anything? (y/n):");
+        s=sc.next();
+
+        switch (s){
+            case "y":
+                System.out.println("PLease type in the number you want " + result + " to be multiplied with:");
+                mult=sc.nextFloat();
+                result*=mult;
+                break;
+            case "n":
+                break;
+                default:
+                    System.out.println("Cancelled...");
+        }
+        System.out.println("final result: " + result);
     }
 }
