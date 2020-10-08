0393c00eb2387b0383c06e4e4f249b231d5a7e41
Elijah Bretbacher <elijah.bretbacher@htl-wels.at>
2020-10-08 12:10:46 +0200

    finished basic implementation

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        float x;
+        float y;
+        float result;
+        Scanner sc=new Scanner(System.in);
 
+        System.out.println("Please enter 2 Numbers to add!");
+        x=sc.nextFloat();
+        y=sc.nextFloat();
+
+        result=x+y;
+        System.out.println("Result: " + result);
     }
 }
